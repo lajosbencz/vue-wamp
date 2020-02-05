9496d636c2f9717562b6ca953867f977b6d733a5
lazos <lazos@lazos.me>
2020-02-06 00:13:10 +0100

    types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,27 @@
+import Vue from 'vue';
 import Connection from "./Connection";
 import Context from "./Context";
-import './plugin'
 
 interface Options {
     auto_reestablish?: boolean,
     auto_close_timeout?: number,
+
     [key: string]: any,
+}
+
+declare module 'vue/types/vue' {
+    interface VueConstructor {
+        $wamp: Context,
+    }
+}
+
+declare module 'vue/types/options' {
+    interface ComponentOptions<V extends Vue> {
+        wamp?: {
+            subscribe?: {},
+            register?: {},
+        }
+    }
 }
 
 export {
